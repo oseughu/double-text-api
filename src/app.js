cb3b6b00f02ab51e8941597afcb07164eabb5988
Ose Ughu <oseughu@gmail.com>
2022-11-16 14:52:07 +0100

    remove localhost from cors origin

--- src/app.js
+++ src/app.js
@@ -23,7 +23,6 @@ app.use(
     origin: [
       'http://localhost:5173',
       'http://localhost:5174',
-      'http://localhost:8080',
       'http://localhost:3000',
       'http://localhost:5000'
     ],
