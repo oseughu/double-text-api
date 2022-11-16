f0040dbd6c644665f18ac58c8908c982d872d6d8
Ose Ughu <oseughu@gmail.com>
2022-11-16 18:31:10 +0100

    set cors origin to true

--- src/app.js
+++ src/app.js
@@ -20,14 +20,7 @@ app.use(cookieParser())
 
 app.use(
   cors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:5174',
-      'http://localhost:8080',
-      'http://localhost:3000',
-      'http://localhost:5000',
-      process.env.CLIENT_URL
-    ],
+    origin: true,
     credentials: true
   })
 )
