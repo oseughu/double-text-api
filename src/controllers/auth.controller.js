930e69c748673a15babe7e4663f35519719e556b
Ose Ughu <oseughu@gmail.com>
2022-11-16 21:35:00 +0100

    add res.headers

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -39,6 +39,10 @@ export const login = asyncHandler(async (req, res) => {
 
   const token = jwt.sign({ id: user._id }, process.env.SECRET)
 
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Credentials', true)
+
   res.cookie('jwt', token, {
     httpOnly: true,
     signed: true,
