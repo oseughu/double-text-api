3f7378baca079952a8c33455e2f0ec26c9238d37
Ose Ughu <oseughu@gmail.com>
2022-11-17 18:11:16 +0100

    remove res.headers

--- src/app.js
+++ src/app.js
@@ -38,21 +38,6 @@ app.set('trust proxy', 1)
 
 app.use(errorHandler)
 
-app.all('/*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', req.headers.origin)
-  res.header('Access-Control-Allow-Credentials', true)
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type,Accept,X-Access-Token,X-Key,Authorization,X-Requested-With,Origin,Access-Control-Allow-Origin,Access-Control-Allow-Credentials'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-  } else {
-    next()
-  }
-})
-
 app.use('/api', routes)
 
 app.get('/', (req, res) => {
