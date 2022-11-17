94e02a5b2b8196e62b795d82200afad9e08ec45d
Ose Ughu <oseughu@gmail.com>
2022-11-17 16:15:56 +0100

    add cors fix to app.js

--- src/app.js
+++ src/app.js
@@ -44,6 +44,21 @@ app.get('/', (req, res) => {
   res.json({ msg: 'Welcome to the Double Text API.' })
 })
 
+app.all('/*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin)
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type,Accept,X-Access-Token,X-Key,Authorization,X-Requested-With,Origin,Access-Control-Allow-Origin,Access-Control-Allow-Credentials'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+  } else {
+    next()
+  }
+})
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`.bgBlue)
 })
